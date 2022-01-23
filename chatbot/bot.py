e6ce17cb4bfedee0453f4d91cd423d505232432b
verxia <76971655+verxia@users.noreply.github.com>
2022-01-23 07:26:43 -0500

    formatting msgs

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -45,6 +45,7 @@ def bot():
 
     greetings = ["hi", "hello", "hey", "helloo", "hellooo", "g morining", "gmorning", "good morning", "morning", "good day", "good afternoon", "good evening", "greetings", "greeting", "good to see you", "its good seeing you", "how are you", "how're you", "how are you doing", "how ya doin'", "how ya doin", "how is everything", "how is everything going", "how's everything going", "how is you", "how's you", "how are things", "how're things", "how is it going", "how's it going", "how's it goin'", "how's it goin", "how is life been treating you", "how's life been treating you", "how have you been", "how've you been", "what is up", "what's up", "what is cracking", "what's cracking", "what is good", "what's good", "what is happening", "what's happening", "what is new", "what's new", "what is neww", "g’day", "howdy"]
 
+    msg.body("--------------------------")
     for word in greetings:
         if word in incoming_msg and not responded:
             msg.body("Hello! How can I help you? Type \"about\" for instructions")
